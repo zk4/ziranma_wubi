a4140bb42c961e0bfe243108fd8e447e19f66469
zk <liuzq7@gmail.com>
2023-12-12 18:38:37 +0800

    只过滤简体

--- convert_wubi.py
+++ convert_wubi.py
@@ -1,33 +1,60 @@
 
 
 # tranditional : simple  chinese  table
-ts_dict = {}
-def mk_ts_dict():
-    global ts_dict
-    with open("./STCharacters.txt",encoding='utf-8') as f:
-        lines = f.readlines()
-        for line in lines:
-            tokens = line.split("\t")
-            print(len(tokens))
-            ts_dict[tokens[1].strip()] = tokens[0].strip()
+# ts_dict = {}
+# def mk_ts_dict():
+#     global ts_dict
+#     with open("./STCharacters.txt",encoding='utf-8') as f:
+#         lines = f.readlines()
+#         for line in lines:
+#             tokens = line.split("\t")
+#             print(len(tokens))
+#             ts_dict[tokens[1].strip()] = tokens[0].strip()
 
-mk_ts_dict()
-print(ts_dict)
+# mk_ts_dict()
+# print(ts_dict)
 
 
+# words = {}
+# with open("./wb_single_table.txt",encoding='utf-8') as f:
+#     lines = f.readlines()
+#     for line in lines:
+#         tokens = line.split(" ")
+#         for token in tokens[1:]:
+#             print(token.strip())
+#             target = token.strip()
+#             words[target]=tokens[0].strip()
+
+# with open("./shouxin_wubi.txt","w",encoding="utf-16") as f:
+#     for k,v in words.items():
+#         f.write(f"{k}={v[:1]}\n")
+
+
+ 
 words = {}
-with open("./wb_single_table.txt",encoding='utf-8') as f:
+
+# 只判断简体字
+def is_simplified(char):
+    try:
+        char.encode('gb2312')
+        return True
+    except UnicodeEncodeError:
+        return False
+ 
+with open("./wb_single_table.txt", encoding='utf-8') as f:
     lines = f.readlines()
     for line in lines:
         tokens = line.split(" ")
         for token in tokens[1:]:
-            print(token.strip())
             target = token.strip()
-            # if token.strip() in ts_dict:
-            #     target = ts_dict[token.strip()]
-            words[target]=tokens[0].strip()
 
-with open("./shouxin_wubi.txt","w",encoding="utf-16") as f:
-    for k,v in words.items():
+            # Check if the character is simplified
+            if is_simplified(target):
+                words[target] = tokens[0].strip()
+            else:
+                print(target)
+with open("./shouxin_wubi.txt", "w", encoding="utf-16") as f:
+    for k, v in words.items():
         f.write(f"{k}={v[:1]}\n")
+        # 最好不要使用两个码判断，跟双拼冲突比较大，一个码足够了。
         # f.write(f"{k}={v[:2]}\n")
